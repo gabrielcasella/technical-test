3642a60c5e0224c2f6e65d0e949f395e421cb696
gabriel <gabriel@peazie.com>
2019-05-22 22:26:32 +1000

    Write application unit test

--- test/unitTest.js
+++ test/unitTest.js
@@ -1,8 +1,31 @@
-var assert = require('assert');
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal([1, 2, 3].indexOf(4), -1);
+'use strict';
+
+var expect = require( 'chai' ).expect;
+
+var myApplication = require( '../application/application' );
+
+describe( 'myLambda', function() {
+    it( `successful invocation`, function( done ) {
+
+        var context = {
+            succeed: function( result ) {
+                expect( result.body ).to.be.a('string').that.include('Hello World!');
+                done();
+            },
+            fail: function() {
+                done(new Error('never context.fail'));
+            }
+        }
+
+        myLambda.handler( {}, { /* context */ }, (err, result) => {
+            try {
+                expect( err ).to.not.exist;
+                expect( result ).to.exist;
+                expect( result.body ).to.be.a('string').that.include('Hello World!');
+                done();
+            } catch( error ) {
+                done( error );
+            }
         });
     });
 });
