b6716416c135e326b9dd49dc9968e7ee531140d5
gabriel <gabriel@peazie.com>
2019-05-23 00:42:22 +1000

    Clean code

--- health/health.js
+++ health/health.js
@@ -8,16 +8,17 @@ AWS.config.update({
 
 var dynamodb = new AWS.DynamoDB();
 
+// Calculate latency
 function getTiming(startTime, endTime) {
-    var secondDiff = endTime[0] - startTime[0];
-    var nanoSecondDiff = endTime[1] - startTime[1];
-    var diffInNanoSecond = secondDiff * NS_PER_SEC + nanoSecondDiff;
+    var secondDiff = endTime[0] - startTime[0],
+        nanoSecondDiff = endTime[1] - startTime[1],
+        diffInNanoSecond = secondDiff * NS_PER_SEC + nanoSecondDiff;
 
     return diffInNanoSecond / MS_PER_NS;
 }
 
-exports.handler = (event, context, callback) => {
-
+// Check the database connection by listing the tables and checking the application table is present
+function checkDatabaseConnection() {
     var table = process.env.TableName;
 
     var dynamoDb_checks = {
@@ -28,72 +29,89 @@ exports.handler = (event, context, callback) => {
         latency: undefined
     };
 
-    dynamodb.listTables({}, function (err, data) {
-        dynamoDb_checks.endAt = process.hrtime();
-        if (err) {
-            console.log(err, err.stack);
-        } else {
-            dynamoDb_checks.online = true;
-            dynamoDb_checks.tableFound = data.TableNames.includes(table);
-            dynamoDb_checks.latency = getTiming(dynamoDb_checks.startAt, dynamoDb_checks.endAt);
-        }
-
-        var response = {
-            "API": {
-                "online": false,
-                "latency": 0
-            },
-            "Database": {
-                "online": dynamoDb_checks.online,
-                "tableFound": dynamoDb_checks.tableFound,
-                "latency": dynamoDb_checks.latency
+    // Return new promise
+    return new Promise(function(resolve, reject) {
+        dynamodb.listTables({}, function (err, data) {
+            dynamoDb_checks.endAt = process.hrtime();
+            if (err) {
+                reject(err);
+            } else {
+                dynamoDb_checks.online = true;
+                dynamoDb_checks.tableFound = data.TableNames.includes(table);
+                dynamoDb_checks.latency = getTiming(dynamoDb_checks.startAt, dynamoDb_checks.endAt);
+                resolve(dynamoDb_checks);
             }
-        }
+        });
+    });
+}
 
-        var https = require('https');
+// Ping the application API to get the status
+function checkAPIConnection() {
+    var https = require('https');
 
-        var api_checks = {
-            online: undefined,
-            statusCode: undefined,
-            startAt: process.hrtime(),
-            endAt: undefined,
-            latency: undefined
-        };
+    var api_checks = {
+        online: undefined,
+        statusCode: undefined,
+        startAt: process.hrtime(),
+        endAt: undefined,
+        latency: undefined
+    };
 
-        var options = {
-            hostname: process.env.HostName,
-            path: '/' + process.env.Stage + '/?ping=1',
-            method: 'GET'
-        };
-        https.get(options, function(res) {
-            api_checks.online = true;
+    var options = {
+        hostname: process.env.HostName,
+        path: '/' + process.env.Stage + '/?ping=1',
+        method: 'GET'
+    };
+
+    // Return new promise
+    return new Promise(function(resolve, reject) {
+        https.get(options, function (res) {
             api_checks.endAt = process.hrtime();
+            api_checks.online = true;
             api_checks.statusCode = res.statusCode;
             api_checks.latency = getTiming(api_checks.startAt, api_checks.endAt);
-            response["API"] = {
-                "online": api_checks.online,
-                "statusCode": api_checks.statusCode,
-                "latency": api_checks.latency
-            };
-            callback(null, {
-                statusCode: '200',
-                body: JSON.stringify(response),
-            });
-        }).on('error', function(e) {
-            console.log(e);
-            api_checks.online = false;
-            api_checks.statusCode = e.statusCode;
+            resolve(JSON.stringify(api_checks));
+        }).on('error', function (e) {
             api_checks.endAt = process.hrtime();
+            api_checks.online = false;
+            api_checks.statusCode = 400;
             api_checks.latency = getTiming(api_checks.startAt, api_checks.endAt);
-            response["API"] = {
-                "online": api_checks.online,
-                "statusCode": api_checks.statusCode,
-                "latency": api_checks.latency
+            resolve(JSON.stringify(api_checks));
+        });
+    });
+}
+
+exports.handler = (event, context, callback) => {
+
+    var response = {
+        "API": {},
+        "Database": {}
+    };
+
+    var APIConnection = checkAPIConnection(),
+        databaseConnection = checkDatabaseConnection();
+
+
+    APIConnection.then(function(APIResult) {
+        response["API"] = {
+            "online": APIResult.online,
+            "statusCode": APIResult.statusCode,
+            "latency": APIResult.latency
+        };
+
+        databaseConnection.then(function(DBResult) {
+            response["Database"] = {
+                "online": DBResult.online,
+                "tableFound": DBResult.tableFound,
+                "latency": DBResult.latency
             };
+            
             callback(null, {
                 statusCode: '200',
                 body: JSON.stringify(response),
             });
+        }, function(err) {
+            callback(err);
         });
     });
 };
